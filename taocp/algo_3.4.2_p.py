213a685b84ea4c33ff430b911ec2274ef3fb0e2e
dpratt3 <57408823+dpratt3@users.noreply.github.com>
2021-11-25 18:58:07 -0600

    Create algo_3.4.2_p.py

---
+++ taocp/algo_3.4.2_p.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import random
+import math
+
+# An implementation of Algorithm P
+X = [ ]
+X.extend(range(1, 53)) # a deck of cards
+
+def shuffle(X):
+    # P1
+    t = len(X) - 1
+    j = t
+    while j > 1:
+        # P2 : Generate uniformly distributed random number
+        U = random.uniform(0, 1)
+
+        # P3
+        k = math.floor(j * U)
+        store = X[j]
+        X[j] = X[k]
+        X[k] = store
+
+        # P4
+        j -= 1
+
+    return(X)
+
+shuffled_X = shuffle(X)
+print(shuffled_X)
