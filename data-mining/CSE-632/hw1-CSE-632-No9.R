1f9d5d074dc3f256119264d8766542db2170e587
David Pratt <david@midnightmechanism.com>
2023-09-06 14:35:15 -0500

    Bring repo up to date

--- data-mining/CSE-632/hw1-CSE-632-No9.R
+++ data-mining/CSE-632/hw1-CSE-632-No9.R
@@ -9,17 +9,24 @@ X_12hr = c(rep(0, 12), rollmean(X, 12))
 
 # (b) Plot discrete X values together with computed 6- and 12-hour moving averages
 # (MA).
+png('9b.png')
+
 plot(X, main = "Original, Six, and 12 Hour Moving Averages", xlab = "Time", ylab = "Value")
 lines(X_6hr, col = "red")
 lines(X_12hr, col = "blue")
 legend("topright", legend = c("Original Data", "6-Hour MA", "12-Hour MA"), col = c("black", "red", "blue"), lty = 1)
+
+dev.off()
 
 # (c) Plot time-dependent variable X and its 4-h exponential moving average (EMA).
 library(TTR)
 X_4hr_EMA <- ts(EMA(x = X, n = 4))
 
 # Plot the original data and 4-hour EMA:
+png('9c.png')
+
 plot(X_4hr_EMA, main = "Original Data and 4-Hour EMA", xlab = "Time", ylab = "Value")
 lines(X_4hr_EMA, col = "goldenrod")
+legend("topright", legend = c("Original Data", "4-Hour EMA"), col = c("black", "goldenrod"), lty = 1)
 
-legend("topright", legend = c("Original Data", "4-Hour EMA"), col = c("black", "goldenrod"), lty = 1):
+dev.off()
