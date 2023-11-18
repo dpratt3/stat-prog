2661c9f64a930f9b278cbd2be4a09900a6cc3bfe
David Pratt <david@midnightmechanism.com>
2023-11-18 16:49:03 -0600

    Filter matrices before binary transformation

--- data-mining/CSE-632/project3/dbscan.py
+++ data-mining/CSE-632/project3/dbscan.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.cluster import DBSCAN
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+from scipy import ndimage
 from joblib import Parallel, delayed
 
 data = pd.read_csv("sparse_pca_transformed_data.csv")
@@ -11,7 +12,7 @@ std_deviation = data.std().mean()
 
 print(std_deviation)
 
-std_dev_mult = np.arange(0.1, 5, 0.1)
+std_dev_mult = np.arange(0.1, 4, 0.1)
 min_samp_mult = np.arange(1, 30, 1)
 
 def perform_dbscan(row, col, std_dev_mult, std_deviation, min_samp_mult, data):
@@ -21,7 +22,6 @@ def perform_dbscan(row, col, std_dev_mult, std_deviation, min_samp_mult, data):
     # Count the number of clusters excluding noise (-1)
     num_clusters = len(set(labels)) - (1 if -1 in labels else 0)
     return num_clusters
-
 
 df = pd.DataFrame(index=std_dev_mult, columns=min_samp_mult)
 
@@ -46,3 +46,28 @@ print(df)
 # n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 # print("\nNumber of clusters:", n_clusters_)
 
+# Find the largest island
+
+# Convert DataFrame to NumPy array with counts and values
+matrix = df.to_numpy()
+unique_vals, counts = np.unique(matrix, return_counts=True)
+unique_val_count = pd.Series(counts, index=unique_vals, name='Counts')
+unique_val_count_df = pd.DataFrame(unique_val_count)
+unique_val_count_df_sorted = unique_val_count_df.sort_values(by='Counts', ascending=False)
+
+# values of zero and one 
+unique_val_count_df_filtered = unique_val_count_df_sorted[~unique_val_count_df_sorted.index.isin([0, 1]) & (unique_val_count_df_sorted['Counts'] > 4) ]
+
+binary_matrix = np.where(matrix == 3, 1, 0)
+
+print(binary_matrix)
+
+# Label connected components
+labeled_matrix, num_features = ndimage.label(binary_matrix)
+
+# Count the sizes of the different islands
+sizes = [np.sum(labeled_matrix == i) for i in range(1, num_features+1)]
+
+# Find the size of the largest island
+largest_island_size = max(sizes)
+print("Size of the largest island:", largest_island_size)
