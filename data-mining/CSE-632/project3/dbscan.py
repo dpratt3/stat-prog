79dec496dabf11adab2cf3737ee8354c1057928e
David Pratt <david@midnightmechanism.com>
2023-11-18 13:35:31 -0600

    Parallelize computation to find regions of parameter cluster stability

--- data-mining/CSE-632/project3/dbscan.py
+++ data-mining/CSE-632/project3/dbscan.py
@@ -1,35 +1,48 @@
 import pandas as pd
 from sklearn.cluster import DBSCAN
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+from joblib import Parallel, delayed
 
 data = pd.read_csv("sparse_pca_transformed_data.csv")
+data = data.iloc[:, :10]
+
 std_deviation = data.std().mean()
 
 print(std_deviation)
 
-clustering = DBSCAN(eps= 0.5 * std_deviation, min_samples=10).fit(data)
+std_dev_mult = np.arange(0.1, 5, 0.1)
+min_samp_mult = np.arange(1, 30, 1)
 
-# Retrieve labels
-labels = clustering.labels_
+def perform_dbscan(row, col, std_dev_mult, std_deviation, min_samp_mult, data):
+    dbscan_result = DBSCAN(eps=std_dev_mult[row] * std_deviation, min_samples=min_samp_mult[col], metric="manhattan").fit(data)
+    labels = dbscan_result.labels_
+    
+    # Count the number of clusters excluding noise (-1)
+    num_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    return num_clusters
 
-# Print the clusters
-print("Cluster labels:\n", labels)
 
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-print("\nNumber of clusters:", n_clusters_)
+df = pd.DataFrame(index=std_dev_mult, columns=min_samp_mult)
 
-print(clustering)
+# Define a function to run DBSCAN in parallel
+results = Parallel(n_jobs=-1)(delayed(perform_dbscan)(row, col, std_dev_mult, std_deviation, min_samp_mult, data) 
+                              for row in range(len(std_dev_mult)) 
+                              for col in range(len(min_samp_mult)))
 
-import matplotlib.pyplot as plt
-import numpy as np
+# Populate the DataFrame with the results
+for col in range(len(min_samp_mult)):
+    for row in range(len(std_dev_mult)):
+        df.loc[std_dev_mult[row], min_samp_mult[col]] = results[row * len(min_samp_mult) + col]
 
-# Assuming 'data' is your dataset and 'labels' contains the cluster labels assigned by DBSCAN
+print(df)
 
-labels = clustering.labels_
+# # Retrieve labels
+# labels = clustering.labels_
 
-# Plot clusters (assuming you have at least 2 columns in your DataFrame)
-plt.scatter(data.iloc[:, 0], data.iloc[:, 7], c=labels, cmap='viridis')
-plt.title('DBSCAN Clustering')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.colorbar(label='Cluster')
-plt.show()
+# # Print the clusters
+# print("Cluster labels:\n", labels)
+
+# n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+# print("\nNumber of clusters:", n_clusters_)
+
