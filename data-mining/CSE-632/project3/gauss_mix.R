c39a88908d2356a61a44784f2d984855a02c5d57
David Pratt <david@midnightmechanism.com>
2023-11-19 16:30:20 -0600

    Raise roof on grpah to see peak of simulated data

--- data-mining/CSE-632/project3/gauss_mix.R
+++ data-mining/CSE-632/project3/gauss_mix.R
@@ -1,13 +1,22 @@
 library(mixtools)
 
-data = read.csv("~/Downloads/QSAR-data/OX2_training_disguised.csv")
+set.seed(1)
 
-fit <- normalmixEM(data$Act)
+data1 <- read.csv("~/Downloads/QSAR-data/OX2_training_disguised.csv")
+data2 <- read.csv("~/Downloads/QSAR-data/OX2_test_disguised.csv")
+data <- c(data1$Act, data2$Act)
+
+fit <- normalmixEM(data)
 
 simulated_data <- rnormmix(n = 1000, lambda = fit$lambda, mu = fit$mu, sigma = fit$sigma)
 
+# Find top of graph for visualization purposes
+observed_density <- density(data)
+simulated_density <- density(simulated_data)
+max_density <- max(c(observed_density$y, simulated_density$y))
+
 # Create a density plot for observed and simulated data
-plot(density(data$Act), main = "Observed vs. Simulated Data Density")
+plot(density(data), main = "Observed vs. Simulated Data Density", ylim = c(0, max_density))
 lines(density(simulated_data), col = "red")
 legend("topright", legend = c("Observed Data", "Simulated Data"), col = c("black", "red"), lty = 1)
 
