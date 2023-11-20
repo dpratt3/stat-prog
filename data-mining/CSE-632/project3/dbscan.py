de67ab2416dccac543cda7e76f328b1deaddf346
David Pratt <david@midnightmechanism.com>
2023-11-19 18:40:33 -0600

    Brinb dbscan up to date

--- data-mining/CSE-632/project3/dbscan.py
+++ data-mining/CSE-632/project3/dbscan.py
@@ -13,16 +13,24 @@ std_deviation = data.std().mean()
 
 print(std_deviation)
 
-std_dev_mult = np.arange(0.1, 5, 0.1)
-min_samp_mult = np.arange(1, 30, 1)
+std_dev_mult = np.arange(4.8, 10.8, 0.1)
+min_samp_mult = np.arange(4, 8, 1)
 
 def perform_dbscan(row, col, std_dev_mult, std_deviation, min_samp_mult, data):
     dbscan_result = DBSCAN(eps=std_dev_mult[row] * std_deviation, min_samples=min_samp_mult[col], metric="euclidean").fit(data)
     labels = dbscan_result.labels_
+
+    
     
     # Count the number of clusters excluding noise (-1)
     num_clusters = len(set(labels)) - (1 if -1 in labels else 0)
     return num_clusters
+    
+    # unique_labels = set(labels)
+    # if len(unique_labels) == 1 and -1 in unique_labels:
+    #     # Handle case where there are no actual clusters
+    #     return -1  # Or any other value you want to return in this case
+    
     # silhouette = silhouette_score(data, labels)
     # return silhouette
 
@@ -76,3 +84,27 @@ largest_island_size = max(sizes)
 print("Size of the largest island:", largest_island_size)
 
 df.to_csv("grid_search.csv", index = True)
+
+# Plug in optimal values
+optimal_cluster = DBSCAN(eps=2.8 * std_deviation, min_samples=14, metric="euclidean").fit(data)
+optimal_labels = optimal_cluster.labels_
+np.unique(optimal_labels) # inspect labels: -1, 0, 1
+
+# Align labels with response variable to assess fit of gaussian mixture model
+train = pd.read_csv("~/Downloads/QSAR-data/OX2_training_disguised.csv")
+test = pd.read_csv("~/Downloads/QSAR-data/OX2_test_disguised.csv")
+
+train_cols = set(train.columns)
+test_cols = set(test.columns)
+
+if train_cols == test_cols:
+    print("Train and test sets have the same columns.")
+else:
+    print("Train and test sets have different columns.")
+
+df = pd.concat([train, test], ignore_index=True, axis=0)
+
+# Label the data:
+labeled_response = pd.DataFrame({'Act': df['Act'].values}, index=optimal_labels)
+labeled_response.to_csv("labeled_reponse.csv", index = True)
+
