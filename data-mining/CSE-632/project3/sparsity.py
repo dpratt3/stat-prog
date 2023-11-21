dd773f95f8919b4272952f885d7dd69fc6da58c5
David Pratt <david@midnightmechanism.com>
2023-11-20 18:27:38 -0600

    Change title position and graph color

--- data-mining/CSE-632/project3/sparsity.py
+++ data-mining/CSE-632/project3/sparsity.py
@@ -11,12 +11,13 @@ axs = axs.flatten()
 
 # Plot each column in a separate subplot
 for i, column in enumerate(raw_data.columns):
-    axs[i].hist(raw_data[column], bins=100, range=(-30, 30))  
-    axs[i].set_title(f'Column {i+1}', fontsize = 10)  
+    axs[i].hist(raw_data[column], bins=100, range=(-30, 30), color = "green")  
+    #axs[i].set_title(f'Column {i+1}', fontsize = 10)  
     # axs[i].set_xlabel('Values')
     axs[i].set_ylim(ymin=0.001)  # Set y-axis limit to start from a small value above zero  
     axs[i].set_ylabel('Frequency')  
-    axs[i].grid(True)  
+    axs[i].grid(True)
+    axs[i].text(0.5, 0.9, f'Column {i+1}', fontsize=10, ha='left', transform=axs[i].transAxes)  
 
 # Adjust layout and display the plot
 plt.tight_layout()
