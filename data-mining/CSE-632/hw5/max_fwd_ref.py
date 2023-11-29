15388d5f32e41163cd76350ada0a31ea18cb6c9e
David Pratt <david@midnightmechanism.com>
2023-11-28 19:45:31 -0600

    Find maximal forward reference

---
+++ data-mining/CSE-632/hw5/max_fwd_ref.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+traversal_log = np.array(['X', 'Y', 'Z', 'W', 'Y', 'A', 'B', 'C', 
+                            'D', 'Y', 'C', 'D', 'E', 'F', 'D', 'E', 
+                                'X', 'Y', 'A', 'B', 'M', 'N'])
+
+keys = np.unique(traversal_log)
+
+# store indices
+positions = {}
+
+# Return the maximal traversal
+def max_diff(arr):
+    if len(arr) == 0 or len(arr) == 1:
+        return 0
+    else:
+        return np.max(np.diff(arr))
+
+for key in keys:
+    indices = np.where(traversal_log == key)[0]
+    positions[key] = max_diff(indices)
+
+for key, value in positions.items():
+    print(f"{key}:{value}")
